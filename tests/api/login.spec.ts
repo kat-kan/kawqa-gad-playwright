cd40e229d51c58f7ba93052a040047f7e23eba2f
Emilia Miller <emiller@future-processing.com>
2023-10-29 02:09:54 +0200

    tests: add LOGIN tests

---
+++ tests/api/login.spec.ts
@@ -0,0 +1,82 @@
+import { test, expect } from "@playwright/test";
+import { testUser } from "../../src/fixtures/api/auth";
+
+test.describe("Login endpoint tests", async () => {
+  let accessToken = "";
+  const expectedResponseCode = 200;
+
+  test("Login endpoint returns 200OK for correct login credentials", async ({
+    request,
+  }) => {
+    //When POST request is send to LOGIN endpoint
+    const response = await request.post(`${process.env.BASE_URL}/api/login`, {
+      // And request body in JSON format is used with proper login credentials
+      data: {
+        email: testUser.userEmail,
+        password: testUser.userPassword,
+      },
+    });
+
+    //Then status code should be 200
+    const code = response.status();
+    expect(code).toBe(expectedResponseCode);
+
+    // And response body should have "access_token" string
+    const body = await response.json();
+    expect(JSON.stringify(body)).toContain("access_token");
+
+    // And access_token should not be empty
+    expect(body.access_token?.length).toBeGreaterThan(0);
+
+    // saving access token
+    //console.log(JSON.stringify(body));
+    accessToken = `Bearer ${body.access_token}`;
+    //console.log(accessToken);
+  });
+
+  test("Login endpoint returns 401Unauthorized for incorrect login credentials", async ({
+    request,
+  }) => {
+    const incorrectPassword = "wrongPassword";
+    const expectedResponseCode = 401;
+    const expectedErrorMessage = "Incorrect email or password";
+
+    //When POST request is send to LOGIN endpoint
+    const response = await request.post(`${process.env.BASE_URL}/api/login`, {
+      // And request body in JSON format is used with proper login credentials
+      data: {
+        email: testUser.userEmail,
+        password: incorrectPassword,
+      },
+    });
+
+    //Then status code should be 401
+    const code = response.status();
+    expect(code).toBe(expectedResponseCode);
+
+    // And response body should have error message
+    const body = await response.json();
+    expect(body.message).toBe(expectedErrorMessage);
+  });
+
+  // SKIPPED - does not work - 401 is returned, WILL BE REMOVED
+  test.skip("Login endpoint returns 400BadRequest for incorrect JSON format", async ({
+    request,
+  }) => {
+    const expectedResponseCode = 400;
+
+    //When POST request is send to LOGIN endpoint
+    const response = await request.post(`${process.env.BASE_URL}/api/login`, {
+      // And request body in JSON format is used with incorrect login credentials
+      data: {
+        email: '" \" "\\\\<email@email.',
+        password: testUser.userPassword,
+      },
+
+    });
+
+    //Then status code should be 400
+    const code = response.status();
+    expect(code).toBe(expectedResponseCode);
+  });
+});
