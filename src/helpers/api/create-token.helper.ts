641edb5685e1088f9df631cda6c973e665c69226
Emilia Miller <emiller@future-processing.com>
2023-11-06 19:48:12 +0100

    tests: add test helper - generate token and set it in headers

---
+++ src/helpers/api/create-token.helper.ts
@@ -0,0 +1,53 @@
+import { APIRequestContext, request } from "@playwright/test";
+import { testUser } from "../../fixtures/api/auth";
+
+export async function createToken(setEmail?: string, setPassword?: string): Promise<string> {
+  if (!setEmail) {
+    setEmail = testUser.userEmail;
+  }
+  if (!setPassword) {
+    setPassword = testUser.userPassword;
+  }
+
+  let accessToken;
+  let baseURL = process.env.BASE_URL;
+
+  const tokenContextRequest = await request.newContext();
+  const response = await tokenContextRequest.post(`${baseURL}/api/login`, {
+    data: {
+      email: setEmail,
+      password: setPassword,
+    },
+  });
+
+  const body = await response.json();
+  accessToken = body.access_token;
+  console.log(accessToken);
+  return accessToken;
+}
+
+export async function createHeaders(): Promise<APIRequestContext> {
+  let requestHeaders;
+  let setTokenInHeaders;
+
+  setTokenInHeaders = await createToken(testUser.userEmail, testUser.userPassword);
+
+  requestHeaders =
+  {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${setTokenInHeaders}`
+  },
+
+    console.log(requestHeaders);
+  return requestHeaders;
+}
+
+export async function createInvalidHeaders(): Promise<APIRequestContext> {
+  let requestHeaders;
+
+  requestHeaders = {
+    Authorization: "Bearer withInvalidAccessToken",
+  };
+
+  return requestHeaders;
+}
