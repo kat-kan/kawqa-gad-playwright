f803ec582249d951437c06f5ed1692bc140babe7
Adam Cegiełka <adamcegielka@tlen.pl>
2023-11-08 18:27:42 +0100

    test(fix): introduces fixes for the test

--- tests/api/articles-get-by-id.spec.ts
+++ tests/api/articles-get-by-id.spec.ts
@@ -5,20 +5,18 @@ let articleID = 3;
 let articleTitle = "What is agile software development?";
 let articleDate = "2021-07-25";
 
-test("GET/articles/3 - validate article information", async ({ request }) => {
-    const response = await request.get(`${baseURL}/api/articles/${articleID}`);
-    const responseBody = await response.json();
-    
-    expect(response.ok()).toBeTruthy();
-    expect(response.status()).toBe(200);    
-    
-    const expectedData = {
-        "title": articleTitle,
-        "date": expect.stringMatching(articleDate)
-    };
-    
-    expect(responseBody).toMatchObject(expectedData);    
-    expect(responseBody.body).toBeTruthy();
+test.only("GET/articles/ return OK status code by ID", async ({ request }) => {
+  const response = await request.get(`${baseURL}/api/articles/${articleID}`);
+  const responseBody = await response.json();
 
-    console.log(await response.json());
+  expect(response.status()).toBe(200);
+
+  const expectedData = {
+    title: articleTitle,
+    date: expect.stringMatching(articleDate),
+  };
+
+  expect(responseBody).toMatchObject(expectedData);
+
+  console.log(await response.json());
 });
