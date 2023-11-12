6913fdd300148fc0e57e349e9eb2fc30fd2d5ec8
Katarzyna <kankowska.katarzyna@gmail.com>
2023-11-12 12:46:12 +0100

    tests: add 3 more tests: 401 when creating article without user data, 400 for malformed json, 422 for too long values. Cleaning up the code
    * add variables for article data

--- tests/api/articles-post.spec.ts
+++ tests/api/articles-post.spec.ts
@@ -1,41 +1,110 @@
 import { testUser } from "@_src_fixtures_api/auth";
 import { createHeaders } from "@_src_helpers_api/create-token.helper";
-import test, { expect } from "@playwright/test";
+import test, { APIRequest, APIResponse, expect } from "@playwright/test";
 
 test.describe("POST articles tests", async () => {
   let baseURL: string = process.env.BASE_URL;
   let setHeaders: any;
   let articlesEndpoint: string = "/api/articles";
+  let articleTitle: string =
+    "Quick Error Handling Guide: What to Do When Coffee Leaks on Your Keyboard";
+  let articleBody: string =
+    "Ah, the joys of being a programmer - navigating through the intricate world of code with the constant companionship of our trusted caffeinated beverages. But what happens when that comforting cup of coffee turns against us, staging a daring escape onto our precious keyboards? Fear not, for we present to you the Quick Error Handling Guide for such a perilous situation.";
+  let articleDate: string = "2024-06-30T15:44:31Z";
+  let articleImage: string = "coffee.jpg";
 
   test.beforeAll(async () => {
     setHeaders = await createHeaders();
   });
   test("Returns 201 Created after creating article", async ({ request }) => {
-    const response = await request.post(`${baseURL + articlesEndpoint}`, {
-      headers: setHeaders,
-      data: {
-        user_id: testUser.userId,
-        title: "newTitle",
-        body: "newContent",
-      },
-    });
+    const response: APIResponse = await request.post(
+      `${baseURL + articlesEndpoint}`,
+      {
+        headers: setHeaders,
+        data: {
+          user_id: testUser.userId,
+          title: articleTitle,
+          body: articleBody,
+          date: articleDate,
+          image: articleImage,
+        },
+      }
+    );
 
     expect(response.status()).toBe(201);
   });
   test("Returns 400 Bad Request after sending malformed JSON", async ({
     request,
-  }) => {});
-  test.only("Returns 422 Unprocessable content after sending invalid article data", async ({
+  }) => {
+    const malformedJson: string = `{
+        "user_id": "${testUser.userId}",
+        "title: ${articleTitle},  // error: missing closing quotation mark
+        "body": "${articleBody}",
+        "date": "${articleDate}",
+        "image": "${articleImage}"
+    }`;
+    const response: APIResponse = await request.post(
+      `${baseURL + articlesEndpoint}`,
+      {
+        headers: setHeaders,
+        data: malformedJson,
+      }
+    );
+
+    expect(response.status()).toBe(400);
+  });
+  test("Returns 422 Unprocessable content after sending article with missing required information", async ({
     request,
   }) => {
-    const response = await request.post(`${baseURL + articlesEndpoint}`, {
-      headers: setHeaders,
-      data: {
-        user_id: testUser.userId
-        //missing title, body, date, image
-      },
-    });
+    const response: APIResponse = await request.post(
+      `${baseURL + articlesEndpoint}`,
+      {
+        headers: setHeaders,
+        data: {
+          user_id: testUser.userId,
+          title: articleTitle,
+          body: articleBody,
+          //missing date, image - all keys are required
+        },
+      }
+    );
 
     expect(response.status()).toBe(422);
+  });
+  test("Returns 422 Unprocessable content after sending article JSON with too long value", async ({
+    request,
+  }) => {
+    const exceedingLengthTitle: string = "a".repeat(10001);
+    const response: APIResponse = await request.post(
+      `${baseURL + articlesEndpoint}`,
+      {
+        headers: setHeaders,
+        data: {
+          user_id: testUser.userId,
+          title: exceedingLengthTitle,
+          body: articleBody,
+          date: articleDate,
+          image: articleImage,
+        },
+      }
+    );
+
+    expect(response.status()).toBe(422);
+  });
+  test("Returns 401 Unauthorized after sending article JSON with missing userId", async ({
+    request,
+  }) => {
+    const response: APIResponse = await request.post(
+      `${baseURL + articlesEndpoint}`,
+      {
+        headers: setHeaders,
+        data: {
+          title: articleTitle,
+          body: articleBody,
+        },
+      }
+    );
+
+    expect(response.status()).toBe(401);
   });
 });
