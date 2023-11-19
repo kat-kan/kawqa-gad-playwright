e0d6f5e023a6610ac9ee0ed9dc206f8084a21772
Karolina Zakrzewska <karolina.m.zakrzewska@gmail.com>
2023-11-19 20:42:45 +0100

    chore: my local changes

--- src/fixtures/api/auth.ts
+++ src/fixtures/api/auth.ts
@@ -1,5 +1,5 @@
 export const testUser = {
-  userEmail: process.env.USER_EMAIL ?? "[NOT SET]",
-  userPassword: process.env.USER_PASSWORD ?? "[NOT SET]",
-  userId: process.env.USER_ID ?? "[NOT SET]",
+  userEmail: process.env.USER_EMAIL ?? "karolinazak584@gmail.com",
+  userPassword: process.env.USER_PASSWORD ?? "Test123456@",
+  userId: process.env.USER_ID ?? "14",
 };
--- src/helpers/api/create-token.helper.ts
+++ src/helpers/api/create-token.helper.ts
@@ -1,53 +1,57 @@
 import { APIRequestContext, request } from "@playwright/test";
 import { testUser } from "@_src_fixtures_api/auth";
 
-export async function createToken(setEmail?: string, setPassword?: string): Promise<string> {
-    if (!setEmail) {
-        setEmail = testUser.userEmail;
-    }
-    if (!setPassword) {
-        setPassword = testUser.userPassword;
-    }
+export async function createToken(
+  setEmail?: string,
+  setPassword?: string
+): Promise<string> {
+  if (!setEmail) {
+    setEmail = testUser.userEmail;
+  }
+  if (!setPassword) {
+    setPassword = testUser.userPassword;
+  }
 
-    let accessToken;
-    let baseURL = process.env.BASE_URL;
+  let accessToken;
+  let baseURL = process.env.BASE_URL;
+  
+  const tokenContextRequest = await request.newContext();
+  const response = await tokenContextRequest.post(`${baseURL}/api/login`, {
+    data: {
+      email: setEmail,
+      password: setPassword,
+    },
+  });
 
-    const tokenContextRequest = await request.newContext();
-    const response = await tokenContextRequest.post(`${baseURL}/api/login`, {
-        data: {
-            email: setEmail,
-            password: setPassword,
-        },
-    });
-
-    const body = await response.json();
-    accessToken = body.access_token;
-    return accessToken;
+  const body = await response.json();
+  accessToken = body.access_token;
+  return accessToken;
 }
 
 export async function createHeaders(): Promise<APIRequestContext> {
-    let requestHeaders;
-    let setTokenInHeaders;
+  let requestHeaders;
+  let setTokenInHeaders;
 
-    setTokenInHeaders = await createToken(testUser.userEmail, testUser.userPassword);
+  setTokenInHeaders = await createToken(
+    testUser.userEmail,
+    testUser.userPassword
+  );
 
-    requestHeaders =
-    {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${setTokenInHeaders}`
-    },
-
-        console.log(`Request Headers are the following: \n`);
-    console.log(requestHeaders);
-    return requestHeaders;
+  (requestHeaders = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${setTokenInHeaders}`,
+  }),
+    console.log(`Request Headers are the following: \n`);
+  console.log(requestHeaders);
+  return requestHeaders;
 }
 
 export async function createInvalidHeaders(): Promise<APIRequestContext> {
-    let requestHeaders;
+  let requestHeaders;
 
-    requestHeaders = {
-        Authorization: "Bearer withInvalidAccessToken",
-    };
+  requestHeaders = {
+    Authorization: "Bearer withInvalidAccessToken",
+  };
 
-    return requestHeaders;
+  return requestHeaders;
 }
