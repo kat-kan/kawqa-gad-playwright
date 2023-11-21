3d1ae5d767e4fb317ffcd573882a6d2f655d4798
Adam Cegiełka <adamcegielka@tlen.pl>
2023-11-21 19:14:01 +0100

    fix(tests): change name of the tests

--- tests/api/articles-id-get.spec.ts
+++ tests/api/articles-id-get.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, APIResponse } from "@playwright/test";
 
-test.describe("GET/articles/{id} Find article by ID", () => {
+test.describe("GET/articles/{id} Get by ID", () => {
   const baseURL: string = process.env.BASE_URL;
 
   test("returns OK status code and correct article data", async ({ request }) => {
