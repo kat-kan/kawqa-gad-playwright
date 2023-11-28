9f2a2b06a706b8c358142f2e340ec4e3a9c0e84b
Emilia Miller <emiller@future-processing.com>
2023-11-28 19:11:27 +0100

    feat: global-setup file - replace console.log with Winston logConsole

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -1,10 +1,11 @@
 import * as dotenv from 'dotenv';
 import baseURL from '../playwright.config';
+import { logConsole } from "@_src_api/utils/log-levels";
 
 async function globalSetup(): Promise<void> {
   dotenv.config({ override: true });
   if (baseURL === undefined) throw new Error('⚠️  URL: is undefined');
-  console.log('⚠️  URL:', process.env.BASE_URL);
+  logConsole('⚠️  URL:', process.env.BASE_URL);
 }
 
 export default globalSetup;
