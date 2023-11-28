bb4d466f3a5a9342f3fc723770793044f08cde93
Emilia Miller <emiller@future-processing.com>
2023-11-28 19:08:06 +0100

    chore: update global-setup

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -1,7 +1,9 @@
 import * as dotenv from 'dotenv';
+import baseURL from '../playwright.config';
 
 async function globalSetup(): Promise<void> {
   dotenv.config({ override: true });
+  if (baseURL === undefined) throw new Error('⚠️  URL: is undefined');
   console.log('⚠️  URL:', process.env.BASE_URL);
 }
 
