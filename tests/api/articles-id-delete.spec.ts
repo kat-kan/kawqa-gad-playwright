b0103170c89fb92073945957ab4f20bda871a5ca
Adam Cegiełka <adamcegielka@tlen.pl>
2023-12-15 19:41:58 +0100

    fix(test): change and refactor

--- tests/api/articles-id-delete.spec.ts
+++ tests/api/articles-id-delete.spec.ts
@@ -1,14 +1,14 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, APIResponse } from "@playwright/test";
 import { createHeaders } from "@_src_helpers_api/create-token.helper";
-import { testUser } from "@_src_fixtures_api/auth";
+import { testUsers } from "@_src_fixtures_api/auth";
 
-test.describe("DELETE/articles/{id} Deletes an article", () => {
+test.describe("DELETE articles/{id}", () => {
   const baseURL: string = process.env.BASE_URL;
-  const articlesEndpoint: string = "/api/articles/";
+  const articles: string = `${baseURL}/api/articles/`;
   let setHeaders: any;
 
   const articleTitle: string = "New Article";
-  const articleContent: string = "This is the content of the new article.";
+  const articleBody: string = "This is the content of the new article.";
   const articleDate: string = "2024-11-30T15:44:31Z";
   const articleImage: string = "image.jpg";
 
@@ -16,49 +16,39 @@ test.describe("DELETE/articles/{id} Deletes an article", () => {
     setHeaders = await createHeaders();
   });
 
-  test("create an article for delete", async ({ request }) => {
-    const statusCode = 201;
-    const response = await request.post(
-      `${baseURL + articlesEndpoint}`,
-      {
+  test("returns 200 status code when delete an article after create", async ({ request }) => {
+    const expectedResponseCode = 200;  
+    const createResponse: APIResponse = await request.post(articles, {
       headers: setHeaders,
       data: {
-        user_id: testUser.userId,
+        user_id: testUsers.regularUser.id,
         title: articleTitle,
-        body: articleContent,
+        body: articleBody,
         date: articleDate,
         image: articleImage,
       },
+    });      
+    
+    const articleData = await createResponse.json();
+    const articleID = articleData.id;
+
+    const deleteResponse = await request.delete(`${articles + articleID}`, {
+      headers: setHeaders,
     });
 
-    expect(response.status()).toBe(statusCode);
+    expect(deleteResponse.status()).toBe(expectedResponseCode);
   });
 
-  test("delete article by ID 60", async ({ request }) => {
-    const nonExistentArticleID = 60;
-    const statusCode = 200;
-
-    const response = await request.delete(
-      `${baseURL + articlesEndpoint + nonExistentArticleID}`,
-      {
-        headers: setHeaders,
-      }
-    );
-
-    expect(response.status()).toBe(statusCode);
-  });
-
-  test("delete a non-existent article", async ({ request }) => {
+  // Error - Received: 401
+  test("returns 404 status code when delete a non-existent article", async ({ request }) => {
+    test.fail();
     const nonExistentArticleID = -1;
-    const statusCode = 404;
+    const expectedResponseCode = 404;
 
-    const response = await request.delete(
-      `${baseURL + articlesEndpoint + nonExistentArticleID}`,
-      {
-        headers: setHeaders,
-      }
-    );
+    const response = await request.delete(`${articles + nonExistentArticleID}`, {
+      headers: setHeaders,
+    });
 
-    expect(response.status()).toBe(statusCode);
+    expect(response.status()).toBe(expectedResponseCode);
   });
 });
