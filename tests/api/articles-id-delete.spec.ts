89332aa687d21aa37e11bd5327ea32fa32aadf5f
Adam Cegiełka <adamcegielka@tlen.pl>
2024-02-05 19:13:38 +0100

    feat: add changes

--- tests/api/articles-id-delete.spec.ts
+++ tests/api/articles-id-delete.spec.ts
@@ -13,10 +13,12 @@ test.describe("DELETE articles/{id}", () => {
   const articleImage: string = "image.jpg";
 
   test.beforeEach(async () => {
+    // Given: Headers are set for the request
     setHeaders = await createHeaders();
   });
 
   test("returns 200 status code when delete an article after create", async ({ request }) => {
+    // Given: An article is created
     const expectedResponseCode = 200;  
     const createResponse: APIResponse = await request.post(articles, {
       headers: setHeaders,
@@ -32,23 +34,29 @@ test.describe("DELETE articles/{id}", () => {
     const articleData = await createResponse.json();
     const articleID = articleData.id;
 
+    // When: The article is deleted
     const deleteResponse = await request.delete(`${articles + articleID}`, {
       headers: setHeaders,
     });
 
+    // Then: The response status code should be 200
     expect(deleteResponse.status()).toBe(expectedResponseCode);
   });
 
-  // Error - Received: 401
+  // 401 status received - Use the `test.fail();` instruction to mark as a failed test
   test("returns 404 status code when delete a non-existent article", async ({ request }) => {
     test.fail();
+
+    // Given: An article ID that does not exist
     const nonExistentArticleID = -1;
     const expectedResponseCode = 404;
 
+    // When: Attempting to delete the non-existent article
     const response = await request.delete(`${articles + nonExistentArticleID}`, {
       headers: setHeaders,
     });
 
+    // Then: The response status code should be 404
     expect(response.status()).toBe(expectedResponseCode);
   });
 });
