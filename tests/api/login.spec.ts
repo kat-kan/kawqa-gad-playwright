979c12e08f3d930a9f7a1344b426ae568c81bb19
Emilia Miller <emiller@future-processing.com>
2024-02-16 15:56:47 +0100

    tests: login - lint and prettier fixes

--- tests/api/login.spec.ts
+++ tests/api/login.spec.ts
@@ -3,35 +3,22 @@ import { testUsers } from '@_src_fixtures_api/auth';
 import { HttpStatusCode } from '@_src_api/enums/api-status-code.enum';
 
 test.describe('Login endpoint tests', async () => {
-  let accessToken;
   const login = `/api/login`;
-  test('Returns 200 OK for correct login credentials', async ({
-    request,
-  }) => {
-    // When POST request is sent to LOGIN endpoint
+  test('Returns 200 OK for correct login credentials', async ({ request }) => {
+    // When
     const response = await request.post(login, {
-      // And request body is in JSON format with proper login credentials
       data: {
         email: testUsers.regularUser.email,
         password: testUsers.regularUser.password,
       },
     });
-
-    // Then status code should be 200
+    // Then
     const code = response.status();
     expect(code).toBe(HttpStatusCode.Ok);
 
-    // And response body should have "access_token" string
     const body = await response.json();
     expect(JSON.stringify(body)).toContain('access_token');
-
-    // And access_token should not be empty
     expect(body.access_token?.length).toBeGreaterThan(0);
-
-    // saving access token for next tests
-    //console.log(JSON.stringify(body));
-    accessToken = `Bearer ${body.access_token}`;
-    //console.log(accessToken);
   });
 
   test('Returns Unauthorized status code when logging in with incorrect credentials', async ({
@@ -39,22 +26,19 @@ test.describe('Login endpoint tests', async () => {
   }) => {
     const incorrectPassword = 'wrongPassword';
     const expectedErrorMessage = 'Incorrect email or password';
-
-    // When POST request is sent to LOGIN endpoint
+    // When
     const response = await request.post(login, {
-      // And request body in JSON format is used with proper login credentials
       data: {
         email: testUsers.regularUser.email,
         password: incorrectPassword,
       },
     });
 
-    // Then response status code should be 401
+    // Then
     const code = response.status();
     expect(code).toBe(HttpStatusCode.Unauthorized);
 
-    // And response body should have "Incorrect email or password" error message
     const body = await response.json();
-    expect(body.message).toBe(HttpStatusCode.Unauthorized);
+    expect(body.message).toBe(expectedErrorMessage);
   });
 });
