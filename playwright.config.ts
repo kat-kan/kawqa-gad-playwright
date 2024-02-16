427d9e2e3166e5b8b69e10661a2cca69eb603d82
Emilia Miller <emiller@future-processing.com>
2024-02-16 15:05:23 +0100

    chore(pwconfig): update playwright config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-require('dotenv').config();
+
+import * as dotenv from 'dotenv';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -20,7 +21,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['json', { outputFile: './playwright-report.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: process.env.BASE_URL,
