b68d861e07bb6ecad75a4e7327485892261e027b
Emilia Miller <emiller@future-processing.com>
2024-02-19 23:15:35 +0100

    fix(articles-post): replace toBe with toEqual

--- tests/api/comments-post.spec.ts
+++ tests/api/comments-post.spec.ts
@@ -14,7 +14,7 @@ test.describe('POST comments tests', async () => {
     setHeaders = await createHeaders();
   });
 
-  test('Returns 201- Created after creating comment', async ({ request }) => {
+  test('Returns 201 - Created after creating comment', async ({ request }) => {
     //When
     const response: APIResponse = await request.post(comments, {
       headers: setHeaders,
@@ -28,7 +28,7 @@ test.describe('POST comments tests', async () => {
     const responseBody = JSON.parse(await response.text());
     //Then
     expect(response.status()).toBe(HttpStatusCode.Created);
-    expect(responseBody.user_id).toBe(testUsers.regularUser.id);
+    expect(responseBody.user_id).toEqual(testUsers.regularUser.id);
     expect(responseBody.article_id).toBe(article_id);
     expect(responseBody.body).toBe(commentBody);
     expect(responseBody.date).toBe(commentDate);
