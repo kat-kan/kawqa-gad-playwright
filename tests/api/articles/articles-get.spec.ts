295ee589ee2a2e291d0522e17c6ceb08cf3b98a4
Katarzyna <kankowska.katarzyna@gmail.com>
2024-04-04 23:01:36 +0200

    fix(articles-get, comments-get): CR changes

--- tests/api/articles/articles-get.spec.ts
+++ tests/api/articles/articles-get.spec.ts
@@ -5,11 +5,9 @@ test.describe('GET/articles endpoint tests', async () => {
   const articles: string = `/api/articles`;
 
   test('Returns 200 OK for all articles', async ({ request }) => {
-    // Given
-    const expectedStatusCode = HttpStatusCode.Ok;
     // When
     const response: APIResponse = await request.get(articles);
     // Then
-    expect(response.status()).toBe(expectedStatusCode);
+    expect(response.status()).toBe(HttpStatusCode.Ok);
   });
 });
--- tests/api/comments/comments-get.spec.ts
+++ tests/api/comments/comments-get.spec.ts
@@ -2,11 +2,11 @@ import { HttpStatusCode } from '@_src_api/enums/api-status-code.enum';
 import { APIResponse, expect, test } from '@playwright/test';
 
 test.describe('GET/comments endpoint tests', async () => {
-  const articles: string = `/api/comments`;
+  const comments: string = `/api/comments`;
 
   test('Returns 200 OK for all comments', async ({ request }) => {
     // When
-    const response: APIResponse = await request.get(articles);
+    const response: APIResponse = await request.get(comments);
     // Then
     expect(response.status()).toBe(HttpStatusCode.Ok);
   });
