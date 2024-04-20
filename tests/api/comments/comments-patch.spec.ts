e744d7eb19b58e5eac3d6927a81102d23d6fc38c
Justyna Kowalska <justyna-kowalska@outlook.com>
2024-04-20 19:24:43 +0200

    test(comments): add more tests for PATCH/comments

---
+++ tests/api/comments/comments-patch.spec.ts
@@ -0,0 +1,166 @@
+import { HttpStatusCode } from '@_src_api/enums/api-status-code.enum';
+import { testUsers } from '@_src_fixtures_api/auth';
+import {
+  createHeaders,
+  createInvalidHeaders,
+} from '@_src_helpers_api/create-token.helper';
+import { APIResponse, expect, request, test } from '@playwright/test';
+
+test.describe('PATCH comments/{id} endpoint tests', async () => {
+  const comments: string = `/api/comments`;
+  let setHeadersForRegularUser;
+  let setInvalidHeaders;
+  let setNewCommentId;
+  const newComment = 'I changed my mind';
+
+  test.beforeAll(async () => {
+    setHeadersForRegularUser = await createHeaders('regular');
+    setNewCommentId = await createNewComment(setHeadersForRegularUser, 1);
+    setInvalidHeaders = await createInvalidHeaders();
+  });
+
+  test('Returns 200 OK status code when updating comment', async ({
+    request,
+  }) => {
+    // When
+    const response: APIResponse = await request.patch(
+      `${comments}/${setNewCommentId}`,
+      {
+        headers: setHeadersForRegularUser,
+        data: {
+          article_id: 1,
+          user_id: testUsers.regularUser.id,
+          body: newComment,
+        },
+      },
+    );
+    // Then
+    expect(response.status()).toBe(HttpStatusCode.Ok);
+    const body = await response.json();
+    expect(body.body).toBe(newComment);
+  });
+
+  test('Returns 404 NotFound status code when trying to update a non-existing comment', async ({
+    request,
+  }) => {
+    // When
+    const response: APIResponse = await request.patch(`${comments}/0`, {
+      headers: setHeadersForRegularUser,
+      data: {
+        article_id: 1,
+        user_id: testUsers.regularUser.id,
+        body: newComment,
+      },
+    });
+    // Then
+    expect(response.status()).toBe(HttpStatusCode.NotFound);
+  });
+
+  test('Returns 401 Unauthorized status code when trying to update an existing comments with random text as token', async ({
+    request,
+  }) => {
+    // Given
+    const expectedErrorMessage = 'Access token not provided!';
+    // When
+    const response: APIResponse = await request.patch(
+      `${comments}/${setNewCommentId}`,
+      {
+        headers: createInvalidHeaders,
+        data: {
+          article_id: 1,
+          user_id: testUsers.regularUser.id,
+          body: newComment,
+        },
+      },
+    );
+    // Then
+    expect(response.status()).toBe(HttpStatusCode.Unauthorized);
+    const body = await response.json();
+    expect(body.error.message).toBe(expectedErrorMessage);
+  });
+
+  test('Returns 401 Unauthorized status code when trying to update comment added by different user', async ({
+    request,
+  }) => {
+    // Given
+    const expectedErrorMessage = 'Access token for given user is invalid!';
+    // When
+    const response: APIResponse = await request.patch(`${comments}/1`, {
+      headers: setHeadersForRegularUser,
+      data: {
+        article_id: 1,
+        user_id: testUsers.regularUser.id,
+        body: newComment,
+      },
+    });
+    // Then
+    expect(response.status()).toBe(HttpStatusCode.Unauthorized);
+    const body = await response.json();
+    expect(body.error.message).toBe(expectedErrorMessage);
+  });
+
+  test('Returns 400 Bad Request status code when trying to update existing comments with malformed JSON', async ({
+    request,
+  }) => {
+    // Given
+    const malformedJson: string = `{
+      "article_id": 1,
+        "user_id": "a",
+        "body": ${newComment} 
+      }`;
+    // When
+    const response: APIResponse = await request.patch(
+      `${comments}/${setNewCommentId}`,
+      {
+        headers: setHeadersForRegularUser,
+        data: malformedJson,
+      },
+    );
+    // Then
+    expect(response.status()).toBe(HttpStatusCode.BadRequest);
+  });
+
+  test('Returns 422 Unprocessable Entity status code when trying to update an existing comments with a new one with wrong date format', async ({
+    request,
+  }) => {
+    // Given
+    const expectedErrorMessage =
+      'One of field is invalid (empty, invalid or too long) or there are some additional fields: Field validation: \"date\" has invalid format!';
+    // When
+    const response: APIResponse = await request.patch(
+      `${comments}/${setNewCommentId}`,
+      {
+        headers: setHeadersForRegularUser,
+        data: {
+          article_id: 1,
+          user_id: testUsers.regularUser.id,
+          body: `${newComment}`,
+          date: '20424-04-20T11:24:52.370Z',
+        },
+      },
+    );
+    // Then
+    expect(response.status()).toBe(HttpStatusCode.UnprocessableEntity);
+    const body = await response.json();
+    expect(body.error.message).toBe(expectedErrorMessage);
+  });
+});
+
+async function createNewComment(
+  setHeadersForRegularUser,
+  article_id,
+): Promise<number> {
+  const newCommentRequest = await request.newContext();
+  const response: APIResponse = await newCommentRequest.post('/api/comments', {
+    headers: setHeadersForRegularUser,
+    data: {
+      user_id: testUsers.regularUser.id,
+      article_id: article_id,
+      body: 'test',
+      date: '2024-06-30T15:44:31Z',
+    },
+  });
+  const responseBody = JSON.parse(await response.text());
+
+  return responseBody.id;
+}
