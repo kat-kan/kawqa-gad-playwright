3b2a3c37b1092b40825421e39d86d9e5b00db30d
Sławomir Duda-Klimaszewski <slawomir@d-kl.pl>
2024-05-15 23:10:05 +0200

    tests(users): Add test checking sensitive data masks

--- tests/api/users/users-get.spec.ts
+++ tests/api/users/users-get.spec.ts
@@ -4,12 +4,24 @@ import { APIResponse, expect, test } from '@playwright/test';
 test.describe('GET/users endpoint tests', async () => {
   const users: string = '/api/users';
 
-  test('Returns all users with 200 OK - without authorization', async ({
-    request,
-  }) => {
+  test('Returns 200 OK - without authorization', async ({ request }) => {
     // When
     const response: APIResponse = await request.get(users);
     // Then
     expect(response.status()).toBe(HttpStatusCode.Ok);
+  });
+
+  test('Checks masking sensitive user data - without authorization', async ({
+    request,
+  }) => {
+    // When
+    const response: APIResponse = await request.get(users);
+    const responseBody = await response.json();
+    // Then
+    responseBody.forEach((user) => {
+      expect(user.email).toEqual('****');
+      expect(user.lastname).toEqual('****');
+      expect(user.password).toEqual('****');
+    });
   });
 });
