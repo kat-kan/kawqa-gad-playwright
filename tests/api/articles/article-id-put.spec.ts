270d38928a4097d749407b86e75359fc9f77e3b7
Justyna Kowalska <justyna-kowalska@outlook.com>
2024-09-28 23:42:36 +0200

    add more tests with enabled feature

--- tests/api/articles/article-id-put.spec.ts
+++ tests/api/articles/article-id-put.spec.ts
@@ -2,18 +2,21 @@ import { HttpStatusCode } from '@_src_api/enums/api-status-code.enum';
 import { testUsers } from '@_src_fixtures_api/auth';
 import { createHeaders } from '@_src_helpers_api/create-token.helper';
 import { APIResponse, expect, test } from '@playwright/test';
+import { customDate } from 'test-data/shared/date.generator';
 
 test.describe('PUT articles/{id} endpoint tests', async () => {
   const articles = `/api/articles`;
   const randomNumber = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
-  const newTitle = 'How to start writing API tests? ';
+  const newTitle = 'How to start writing API tests?';
   const newTitle2 = 'How to start writing API tests?' + randomNumber;
   const newTitle3 = 'How to start writing good API tests?' + randomNumber;
+  const oldTilte = 'The difference between Scrum and Kanban';
   const newContent = 'Join KawQA z automatu';
-  const articleDate: string = '2024-07-14T15:44:31Z';
+  const articleDate: string = customDate.pastDate;
   const articleImage: string =
     'src\\test-data\\images\\Roasted_coffee_beans.jpg';
   const articleId: number = randomNumber;
+  const articleId2: number = randomNumber + 1;
   const features: string = `/api/config/features`;
   let setHeaders: { [key: string]: string };
 
@@ -140,6 +143,32 @@ test.describe('PUT articles/{id} endpoint tests', async () => {
     expect(response.status()).toBe(HttpStatusCode.UnprocessableEntity);
   });
 
+  test('Returns 200 ok status code when updating article with the same title', async ({
+    request,
+  }) => {
+    const response: APIResponse = await request.put(`${articles}/1`, {
+      headers: setHeaders,
+      data: {
+        user_id: testUsers.regularUser.id,
+        title: oldTilte,
+        body: newContent,
+        date: articleDate,
+        image: articleImage,
+      },
+    });
+    const responseBody = JSON.parse(await response.text());
+
+    expect.soft(response.status()).toBe(HttpStatusCode.Ok);
+    expect
+      .soft(responseBody.user_id.toString())
+      .toEqual(testUsers.regularUser.id.toString());
+    expect.soft(responseBody.title).toBe(oldTilte);
+    expect.soft(responseBody.body).toBe(newContent);
+    expect.soft(responseBody.date).toBe(articleDate);
+    expect.soft(responseBody.image).toBe(articleImage);
+    expect.soft(typeof responseBody.id === 'number').toBe(true);
+  });
+
   test.describe('PUT articles/{id} endpoint tests with enabled feature_validate_article_title', async () => {
     test.beforeAll(async ({ request }) => {
       const response: APIResponse = await request.post(features, {
@@ -158,7 +187,7 @@ test.describe('PUT articles/{id} endpoint tests', async () => {
         headers: setHeaders,
         data: {
           user_id: testUsers.regularUser.id,
-          title: `${newTitle}${randomNumber}`,
+          title: newTitle,
           body: newContent,
           date: articleDate,
           image: articleImage,
@@ -170,18 +199,39 @@ test.describe('PUT articles/{id} endpoint tests', async () => {
       expect
         .soft(responseBody.user_id.toString())
         .toEqual(testUsers.regularUser.id.toString());
-      expect.soft(responseBody.title).toBe(`${newTitle}${randomNumber}`);
+      expect.soft(responseBody.title).toBe(newTitle);
       expect.soft(responseBody.body).toBe(newContent);
       expect.soft(responseBody.date).toBe(articleDate);
       expect.soft(responseBody.image).toBe(articleImage);
       expect.soft(typeof responseBody.id === 'number').toBe(true);
     });
 
+    test('Returns 422 status code when updating article with the same title', async ({
+      request,
+    }) => {
+      const response: APIResponse = await request.put(`${articles}/1`, {
+        headers: setHeaders,
+        data: {
+          user_id: testUsers.regularUser.id,
+          title: oldTilte,
+          body: newContent,
+          date: articleDate,
+          image: articleImage,
+        },
+      });
+      const responseBody = JSON.parse(await response.text());
+
+      expect.soft(response.status()).toBe(HttpStatusCode.UnprocessableEntity);
+      expect
+        .soft(responseBody.error.message)
+        .toBe('Field "title" is not unique!');
+    });
+
     test('Returns 201 Created status code when creating article using PUT', async ({
       request,
     }) => {
       const response: APIResponse = await request.put(
-        `${articles}/${articleId}`,
+        `${articles}/${articleId2}`,
         {
           headers: setHeaders,
           data: {
