0fc84c7ed4feb300ec9d67fba655d08b7642247c
Sławomir Duda-Klimaszewski <slawomir@d-kl.pl>
2024-10-13 16:34:55 +0200

    fix(articles): repair 404 test, refactor

--- tests/api/articles/articles-id-delete.spec.ts
+++ tests/api/articles/articles-id-delete.spec.ts
@@ -6,23 +6,21 @@ import { customDate } from 'test-data/shared/date.generator';
 
 test.describe('DELETE articles/{id}', () => {
   const articles: string = `/api/articles`;
-
-  const articleTitle: string = 'New Article';
-  const articleBody: string = 'This is the content of the new article.';
-  const articleDate: string = customDate.pastDate;
-  const articleImage: string = 'image.jpg';
-
   let setHeaders: { [key: string]: string };
 
   test.beforeAll(async () => {
-    // Given: Headers are set for the request
     setHeaders = await createHeaders();
   });
 
   test('returns 200 status code when delete an article after create', async ({
     request,
   }) => {
-    // Given: An article is created
+    // Given: an article is created
+    const articleTitle: string = 'New Article';
+    const articleBody: string = 'This is the content of the new article.';
+    const articleDate: string = customDate.pastDate;
+    const articleImage: string = 'image.jpg';
+
     const createResponse: APIResponse = await request.post(articles, {
       headers: setHeaders,
       data: {
@@ -33,12 +31,12 @@ test.describe('DELETE articles/{id}', () => {
         image: articleImage,
       },
     });
-    const articleData = await createResponse.json();
-    const articleID = articleData.id;
+    const createdArticle = await createResponse.json();
+    const createdArticleID = createdArticle.id;
 
     // When: The article is deleted
     const deleteResponse: APIResponse = await request.delete(
-      `${articles}/${articleID}`,
+      `${articles}/${createdArticleID}`,
       {
         headers: setHeaders,
       },
@@ -48,15 +46,11 @@ test.describe('DELETE articles/{id}', () => {
     expect(deleteResponse.status()).toBe(HttpStatusCode.Ok);
   });
 
-  // 401 status received - Use the `test.fail();` instruction to mark as a failed test
   test('returns 404 status code when delete a non-existent article', async ({
     request,
   }) => {
-    test.fail();
-
     // Given: An article ID that does not exist
     const nonExistentArticleID = -1;
-    const expectedResponseCode = 404;
 
     // When: Attempting to delete the non-existent article
     const response = await request.delete(
@@ -67,6 +61,6 @@ test.describe('DELETE articles/{id}', () => {
     );
 
     // Then: The response status code should be 404
-    expect(response.status()).toBe(expectedResponseCode);
+    expect(response.status()).toBe(HttpStatusCode.NotFound);
   });
 });
