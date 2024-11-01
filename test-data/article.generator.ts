6ea663eed8cfab1b232717f5bfdb3e465aa733a9
slawekdk <slawomir.duda-klimaszewski@dataedo.com>
2024-11-01 23:35:40 +0100

    fix: refactor of article title generator

--- test-data/article.generator.ts
+++ test-data/article.generator.ts
@@ -4,15 +4,17 @@ import { APIRequestContext, APIResponse } from '@playwright/test';
 export async function generateUniqueArticleTitle(
   request: APIRequestContext,
 ): Promise<string> {
+  // get list of all article titles
   const getAllArticles: APIResponse = await request.get(`/api/articles`, {});
-  const articlesJson = await getAllArticles.json();
+  const articlesJSON = await getAllArticles.json();
   const articleTitleList: string[] = [];
-  for (const articleNumber in articlesJson) {
-    articleTitleList.push(articlesJson[articleNumber].title);
+  for (const articleNumber in articlesJSON) {
+    articleTitleList.push(articlesJSON[articleNumber].title);
   }
+
+  // ensure that the title generated using faker is not equal to any existing article title
   let checker: boolean = true;
   let uniqueArticleTitle: string;
-
   while (checker == true) {
     uniqueArticleTitle = faker.book.title();
     // eslint-disable-next-line playwright/no-conditional-in-test
@@ -20,6 +22,7 @@ export async function generateUniqueArticleTitle(
       checker = false;
     }
   }
+  // TODO: add counter to break the loop after X iterations
   return uniqueArticleTitle;
 }
 
@@ -27,7 +30,7 @@ export async function getExistingArticleTitle(
   request: APIRequestContext,
 ): Promise<string> {
   const getAllArticles: APIResponse = await request.get(`/api/articles`, {});
-  const articlesJson = await getAllArticles.json();
-  const existingArticleTitle: string = articlesJson[1].title;
-  return existingArticleTitle;
+  const articlesJSON = await getAllArticles.json();
+  const firstArticleTitle: string = articlesJSON[0].title;
+  return firstArticleTitle;
 }
