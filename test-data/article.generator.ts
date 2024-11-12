4b8935bbcdd90701d955b822fb3216dec1a59e53
Sławomir Duda-Klimaszewski <slawomir@d-kl.pl>
2024-11-13 00:07:53 +0100

    fix: Modify generateUniqueArticleTitle function according to PR 126

--- test-data/article.generator.ts
+++ test-data/article.generator.ts
@@ -6,23 +6,21 @@ export async function generateUniqueArticleTitle(
 ): Promise<string> {
   // get list of all article titles
   const articlesJSON = await generateArticlesJSON(request);
-  const articleTitleList: string[] = [];
-  for (const articleNumber in articlesJSON) {
-    articleTitleList.push(articlesJSON[articleNumber].title);
-  }
+  const articleTitleList: string[] = articlesJSON.map(
+    (article: { title: string }) => article.title,
+  );
 
   // ensure that the title generated using faker is not equal to any existing article title
   let isUnique: boolean = false;
 
   let uniqueArticleTitle: string;
-  while (isUnique == false) {
+  while (!isUnique) {
     uniqueArticleTitle = faker.lorem.sentence();
     // eslint-disable-next-line playwright/no-conditional-in-test
     if (!articleTitleList.includes(uniqueArticleTitle)) {
       isUnique = true;
     }
   }
-  // TODO: add counter to break the loop after X iterations
   return uniqueArticleTitle;
 }
 
@@ -36,8 +34,8 @@ export async function getExistingArticleTitle(
 
 export async function generateArticlesJSON(
   request: APIRequestContext,
-): Promise<object> {
-  const getAllArticles: APIResponse = await request.get(`/api/articles`, {});
+): Promise<{ id: number; title: string }[]> {
+  const getAllArticles: APIResponse = await request.get(`/api/articles`);
   const articlesJSON = await getAllArticles.json();
   return articlesJSON;
 }
