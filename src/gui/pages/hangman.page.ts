0dd5e3b38ed5c21465e216d15f63c1dca428fab4
Sławomir Duda-Klimaszewski <slawomir@d-kl.pl>
2025-02-16 15:27:34 +0100

    test: expand the starting game test

--- src/gui/pages/hangman.page.ts
+++ src/gui/pages/hangman.page.ts
@@ -4,11 +4,22 @@ import { Page } from '@playwright/test';
 export class HangmanPage extends BasePage {
   url = '/games/hangman.html';
   hangmanPicture = this.page.locator('#hangman');
-  soughtWord = this.page.locator('#word');
+  mySolutionWord = this.page.locator('#word');
   startButton = this.page.getByTestId('startButton');
   letterRow = this.page.locator('#letters');
 
   constructor(page: Page) {
     super(page);
+  }
+
+  async clickStartButtonWithGameSolution(endpointUrl: string): Promise<string> {
+    const responsePromise = this.page.waitForResponse((response) => {
+      return response.url().includes(endpointUrl) && response.status() === 200;
+    });
+    await this.startButton.click();
+    const response = await responsePromise;
+    const responseJson = await response.json();
+    const finalWord = responseJson.word;
+    return finalWord;
   }
 }
--- tests/gui/games/hangman.spec.ts
+++ tests/gui/games/hangman.spec.ts
@@ -23,13 +23,17 @@ test('Starting the game @logged', async ({ page }) => {
   await page.goto(hangmanPage.url);
 
   // When
-  await hangmanPage.startButton.click();
+  const gameSolution = await hangmanPage.clickStartButtonWithGameSolution(
+    '/api/hangman/random',
+  );
+  const mySolution = await hangmanPage.mySolutionWord.textContent();
 
   // Then
   await expect
     .soft(hangmanPage.hangmanPicture)
     .toHaveText('_________\n|\n|\n|\n|\n|\n_|_');
-  await expect.soft(hangmanPage.soughtWord).not.toContainText(/[A-Z]/);
-  await expect.soft(hangmanPage.soughtWord).toContainText('_ ');
+  expect.soft(mySolution).not.toMatch(/[A-Z]/);
+  expect.soft(mySolution).toContain('_ ');
+  expect.soft(mySolution).toHaveLength(gameSolution.length * 2 - 1);
   await expect.soft(hangmanPage.letterRow).toBeVisible();
 });
