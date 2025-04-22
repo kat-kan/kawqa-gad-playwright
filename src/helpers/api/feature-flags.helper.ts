f6cdf65158514408b91cc2b61fe9c96db2005388
Sławomir Duda-Klimaszewski <slawomir@d-kl.pl>
2025-04-22 22:22:27 +0200

    docs: JSDoc for enableFeatureFlag method

--- src/helpers/api/feature-flags.helper.ts
+++ src/helpers/api/feature-flags.helper.ts
@@ -2,6 +2,17 @@ import { HttpStatusCode } from '@_src_api/enums/api-status-code.enum';
 import { FeatureFlags } from '@_src_api/enums/feature-flags.enum';
 import { APIRequestContext, APIResponse, expect } from '@playwright/test';
 
+/**
+ * Enables or disables the selected feature flags for API tests
+ * @param request Pass `request` fixture to the function
+ * @param flagName Name of the flag from `FeatureFlags` enum, e.g. FeatureFlags.labels to select `feature-labels` flag
+ * @param isEnabled `True`: enable flag, `False`: disable flag
+ *
+ * @example
+ * ```
+ * await enableFeatureFlag(request, FeatureFlags.labels, false);
+ * ```
+ */
 export async function enableFeatureFlag(
   request: APIRequestContext,
   flagName: FeatureFlags,
